124558e2b01ee3f060658d85e006c0f3a9539bdc
Danielpm2 <danielsalinas0077@gmail.com>
2026-03-03 19:36:27 -0500

    changement de nom

--- js/apps/welcome.js
+++ js/apps/welcome.js
@@ -15,7 +15,7 @@ APP_REGISTRY.push({
       <div class="welcome-logo">
         <img src="icons/windows_vista/vista_get_started.ico" width="80" height="80" alt="Welcome" draggable="false">
       </div>
-      <h1>Daniel OS</h1>
+      <h1>my PortfoliOS</h1>
       <p>Montreal based Software Engineering Student</p>
       <p style="margin-top:6px;font-size:12px;color:#7090b0">
         More on this site to come :).<br>
